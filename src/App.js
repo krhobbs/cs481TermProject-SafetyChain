138d17c08a0e1ed970c3c64bee4df7681ad54c42
Kyle Hobbs <kyle.hobbs01@gmail.com>
2020-04-08 16:49:34 -0600

    Use navigator.geolocation API to get the user's current location - Does not work on Safari due to security/privacy restrictions with the http protocol

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: 0,
+      longitude: 0
+    }
+  }
+
+  componentDidMount() {
+    let currentState = this;
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      currentState.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+      console.log("Latitude is ", position.coords.latitude);
+      console.log("Longitude is ", position.coords.longitude);
+    });
+  }
+
+  render () {
+    return (
+        <div className="App">
+          <h4>Your latitude: {this.state.latitude}</h4>
+          <br />
+          <h4>Your longitude: {this.state.longitude}</h4>
+
+        </div>
+    );
+  }
 }
 
 export default App;
