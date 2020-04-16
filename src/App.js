81da015834aeee8b739f9c14ec34f42bca52fed4
Kyle Hobbs <kyle.hobbs01@gmail.com>
2020-04-16 15:25:24 -0600

    Function to display a list of traffic violations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Violation from "./components/violation";
 
 class App extends Component {
   constructor(props) {
@@ -10,7 +11,7 @@ class App extends Component {
       longitude: 0,
       speed: 0,
       speedLimit: 60,
-      violations: []
+      violations: [] // List of violation objects containing: speed, speedLimit, date, lat, lon
     }
   }
 
@@ -26,12 +27,19 @@ class App extends Component {
     });
   }
 
+  displayViolations() {
+    return this.state.violations.map((violation) =>
+        <Violation speed={violation.speed} speedLimit={violation.speedLimit} date={violation.date} lat={violation.lat} lon={violation.lon} />
+    );
+  }
+
   render () {
     return (
         <div className="App">
           <h4>Your latitude: {this.state.latitude}</h4>
           <br />
           <h4>Your longitude: {this.state.longitude}</h4>
+          {this.displayViolations()}
         </div>
     );
   }
