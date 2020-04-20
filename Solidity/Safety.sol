45d7f02e42bb5d27529dd8c6348689e2a0da2749
Owen Davidson <thenewesemgeez@gmail.com>
2020-04-20 16:49:11 -0600

    added Safety.sol
    Added basic solidity contract to track traffic infractions

---
+++ Solidity/Safety.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract Safety {
+
+    struct Infraction {
+        uint16 limit;
+        uint16 speed;
+        uint datetime;
+        uint32 latitude;
+        uint32 longitude;
+    }
+
+    event NewInfraction(uint id, uint datetime);
+
+    Infraction[] public infractions;
+
+    mapping(address => uint) public userInfractionCount;
+    mapping(uint => address) public infractionToUser;
+
+    function logInfraction(uint16 _limit, uint16 _speed, uint _datetime, uint32 _lat, uint32 _lon) public {
+        uint id = infractions.push(Infraction({limit: _limit, speed: _speed, datetime: _datetime, latitude: _lat, longitude: _lon})) - 1;
+        infractionToUser[id] = msg.sender;
+        userInfractionCount[msg.sender] = userInfractionCount[msg.sender] + 1;
+        emit NewInfraction(id,_datetime);
+    }
+}
