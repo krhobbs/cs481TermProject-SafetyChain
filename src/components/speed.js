89254cb3c475d12f65f9ddeaa087fa4bf1e3b30f
ebeaney <ebeaney@rams.colostate.edu>
2020-04-21 00:02:41 -0600

    basic speed

--- src/components/speed.js
+++ src/components/speed.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react';
 class Speed extends Component {
 
 
+
     render () {
         return (
             // Want to display the speed over speed limit, passed as props
+            //Coordinates added for testing purposes
             <div className="speed">
-                <h1>{this.props.speed} / <span style={{fontSize: "12px"}}>{this.props.speedLimit} mph</span></h1>
+                <h1>{this.props.speed} mph / <span style={{fontSize: "12px"}}>{this.props.speedLimit} mph</span></h1>
+                <h1>{this.props.longitude} , {this.props.latitude}</h1>
             </div>
         );
     }
