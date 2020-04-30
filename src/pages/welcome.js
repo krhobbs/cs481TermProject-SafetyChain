51772ad3ef6c407937d1f782e7ac9fb3ae7a0cae
Owen Davidson <thenewesemgeez@gmail.com>
2020-04-30 14:35:49 -0600

    added an 's' for grammar purposes

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -14,7 +14,7 @@ class Welcome extends Component {
         return (
             <div>
                 <h2> About SafetyChain </h2>
-                <h3>SafetyChain is an application designed to use location data to check for speeding. It is unique in that it records are stored in the blockchain, and thus cannot be changed by anyone, allowing for fair and transparent records between the client and the agent. </h3>
+                <h3>SafetyChain is an application designed to use location data to check for speeding. It is unique in that its records are stored in the blockchain, and thus cannot be changed by anyone, allowing for fair and transparent records between the client and the agent. </h3>
                 <h3>Customers: to check past speeding violations, use the "Customer Portal". </h3>
                 <h3>Agents: use the "Agency Portal" to get started. </h3>
             </div>
